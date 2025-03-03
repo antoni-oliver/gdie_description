0f7efed2d9be571ffc714b937919de8139a2b477
Antoni Oliver <antoni.oliver.tomas@gmail.com>
2025-03-03 15:35:52 +0100

    whisper.py - tiny

--- whisper.py
+++ whisper.py
@@ -1,3 +1,8 @@
+import time
+
+start = time.time()
 from transformers import pipeline
-transcriber = pipeline(model="openai/whisper-large-v2", torch_dtype="auto")
+transcriber = pipeline(model="openai/whisper-tiny", torch_dtype="auto")
 print(transcriber('audio2.m4a', return_timestamps=True, generate_kwargs={"language": "english"}))
+end = time.time()
+print(end - start)
