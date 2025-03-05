ecff1f93d5d13623ab42f262dccc35688dac3ddb
Antoni Oliver <antoni.oliver.tomas@gmail.com>
2025-03-05 18:52:12 +0100

    Whisper chunking

--- whisper.py
+++ whisper.py
@@ -2,7 +2,7 @@ import time
 
 start = time.time()
 from transformers import pipeline
-transcriber = pipeline(model="openai/whisper-tiny", torch_dtype="auto")
-print(transcriber('audio2.m4a', return_timestamps=True, generate_kwargs={"language": "english"}))
+transcriber = pipeline(model="openai/whisper-tiny", torch_dtype="auto", chunk_length_s=10)
+print(transcriber('audio2.m4a', batch_size=8, return_timestamps=True))
 end = time.time()
 print(end - start)
